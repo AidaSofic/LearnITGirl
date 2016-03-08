953770c9cb306578b9d566b64a930929b4fe0648
AidaSofic <ssalihbegovic.aida@gmail.com>
2016-03-08 13:11:25 +0100

    Increment Operator and Assignment Operator
    Increment Operator and Assignment Operator

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x=10;
+       // x++;
+        //x=x+1
+        //System.out.println(x++); postincrement
+       // System.out.println(++x); preincrement
+        //System.out.println(x);
+        //x +=5; //x = x+5;
+        //x -=5; // x = x-5;
+        //x /=5; // x = x/5;
+        x *=5; //x=x*5:
+        System.out.println(x);
+
+    }
+}
